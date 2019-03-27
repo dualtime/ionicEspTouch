11d5eb4440a662562987cfb4b2f6af7ec817c850
Diogo Matias <diogomatias4aca@hotmail.com>
2019-03-27 11:17:18 +0000

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare let esptouch;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,4 +12,30 @@ export class HomePage {
 
   }
 
+  startConfig() {
+    esptouch.start(
+      "MBF_Lounge",
+      "00:50:7f:f1:17:cc",
+      "bfmafra2016",
+      "0",
+      "1",
+      (res: any) => {
+        console.log(res)
+      },
+      (err: any) => {
+        console.log(err)
+      }
+    )
+  }
+
+  stopConfig() {
+    esptouch.stop(
+      (res: any) => {
+        console.log(res)
+      },
+      (err: any) => {
+        console.log(err)
+      }
+    )
+  }
 }
